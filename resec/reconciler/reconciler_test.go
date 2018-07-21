e0e639345f710c9bacc13a66c153dd73a049b11b
Christian Winther <jippignu@gmail.com>
2018-07-21 10:18:57 +0200

    more test docs
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- resec/reconciler/reconciler_test.go
+++ resec/reconciler/reconciler_test.go
@@ -27,18 +27,20 @@ func TestReconciler_RunBecomeMaster(t *testing.T) {
 		withRedis(state.Redis{Ready: true, Connected: true}).
 		eval(ResultNoMasterElected)
 
-	// consul updated state, we know hold the master lock, so
+	// consul updated state, we know hold the master lock, so configure redis to become master
 	helper.
 		withConsulState(state.Consul{Ready: true, Healthy: true, Master: true}).
 		expectConsulCommands("register_service").
 		expectRedisCommands("run_as_master").
 		eval(ResultRunAsMaster)
 
+	// redis updated state, its now running as a master node, so all we need to do is update the service
 	helper.
 		withRedis(state.Redis{Ready: true, Connected: true, Replication: state.RedisReplicationState{Role: "master"}}).
 		expectConsulCommands("update_service").
 		eval(ResultUpdateService)
 
+	// no state change, so all we need to do is update the service
 	helper.
 		expectConsulCommands("update_service").
 		eval(ResultUpdateService)
